a39aa3604cf80cbc7543d4a5372634b32fd3c6e4
walterorellana <worellanab1@miumg.edu.gt>
2023-09-01 20:46:45 -0600

    Cambio

--- EXAMEN/js/CallAjax.js
+++ EXAMEN/js/CallAjax.js
@@ -1,43 +1,46 @@
 
+let myDoughnutChart;
+LLamada2();
 
-var url = "https://censopoblacion.gt/indicadores/2/999";
+
+// var url = "https://censopoblacion.gt/indicadores/2/999";
 
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    const hombresElement = document.getElementById("Tsm");
-    var mujeresElement = document.getElementById("Tsf");
-    var age014Element = document.getElementById("age14");
-    var age1564Element = document.getElementById("age15");
-    var age65PlusElement = document.getElementById("age65");
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     const hombresElement = document.getElementById("Tsm");
+//     var mujeresElement = document.getElementById("Tsf");
+//     var age014Element = document.getElementById("age14");
+//     var age1564Element = document.getElementById("age15");
+//     var age65PlusElement = document.getElementById("age65");
 
-    hombresElement.innerText = "Hombres: " + data[0].total_sexo_hombre;
-    mujeresElement.innerText = "Mujeres: " + data[0].total_sexo_mujeres;
-    age014Element.innerText = "0-14 años: " + data[0].pob_edad_014;
-    age1564Element.innerText = "15-64 años: " + data[0].pob_edad_1564;
-    age65PlusElement.innerText = "65 y más años: " + data[0].pob_edad_65;
+//     hombresElement.innerText = "Hombres: " + data[0].total_sexo_hombre;
+//     mujeresElement.innerText = "Mujeres: " + data[0].total_sexo_mujeres;
+//     age014Element.innerText = "0-14 años: " + data[0].pob_edad_014;
+//     age1564Element.innerText = "15-64 años: " + data[0].pob_edad_1564;
+//     age65PlusElement.innerText = "65 y más años: " + data[0].pob_edad_65;
 
-    drawPieChart();
-  });
+//     drawPieChart();
+//   });
 
-  function drawPieChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['Edad', 'Cantidad'],
-        ['14 años o menos', parseInt(document.getElementById('age14').innerText.split(":")[1].trim())],
-        ['15 años', parseInt(document.getElementById('age15').innerText.split(":")[1].trim())],
-        ['65 años o más', parseInt(document.getElementById('age65').innerText.split(":")[1].trim())]
-    ]);
+//   function drawPieChart() {
+//     var data = google.visualization.arrayToDataTable([
+//         ['Edad', 'Cantidad'],
+//         ['14 años o menos', parseInt(document.getElementById('age14').innerText.split(":")[1].trim())],
+//         ['15 años', parseInt(document.getElementById('age15').innerText.split(":")[1].trim())],
+//         ['65 años o más', parseInt(document.getElementById('age65').innerText.split(":")[1].trim())]
+//     ]);
 
-    var options = {
-        title: 'Distribución por edad',
-        pieHole: 0.4,
-    };
+//     var options = {
+//         title: 'Distribución por edad',
+//         pieHole: 0.4,
+//     };
 
-    var chart = new google.visualization.PieChart(document.getElementById('pieChart'));
-    chart.draw(data, options);
-}
+//     var chart = new google.visualization.PieChart(document.getElementById('pieChart'));
+//     chart.draw(data, options);
+// }
 
 
 
@@ -51,7 +54,7 @@ function LLamada1() {
       "https://censopoblacion.gt/indicadores/2/" +
       (cmbUbication.value == "Mostrar Todos" ? "" : cmbUbication.value);
   } else {
-    url = "https://censopoblacion.gt/indicadores/2/999";
+    url = "https://censopoblacion.gt/indicadores/1/999";
   }
 
   fetch(url)
@@ -106,7 +109,11 @@ function LLamada2() {
     });
 }
 
+
+
 function grafica(hombres, mujeres) {
+  const ctx = document.getElementById('myDoughnutChart').getContext('2d');
+
   const data = {
     labels: ['Hombres', 'Mujeres'],
     datasets: [{
@@ -120,9 +127,21 @@ function grafica(hombres, mujeres) {
     }]
   };
 
-  const ctx = document.getElementById('myDoughnutChart').getContext('2d');
-  const myDoughnutChart = new Chart(ctx, {
-    type: 'doughnut',
-    data: data,
-  });
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+
+  if (myDoughnutChart) {
+    // Si la instancia de la gráfica ya existe, actualiza los datos y la gráfica
+    myDoughnutChart.data.datasets[0].data = [hombres, mujeres];
+    myDoughnutChart.update(); // Actualiza la gráfica con los nuevos datos
+  } else {
+    // Si la instancia de la gráfica no existe, crea una nueva instancia
+    myDoughnutChart = new Chart(ctx, {
+      type: 'doughnut',
+      data: data,
+      options: options
+    });
+  }
 }
